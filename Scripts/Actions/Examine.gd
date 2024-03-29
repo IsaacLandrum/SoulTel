11c5532c262c417cc3375f4b84455a0cdc602ad8
czajkatony <4tonyc4@gmail.com>
2024-03-28 19:38:07 -0500

    Action Wheel works with debug statements

--- Scripts/Actions/Examine.gd
+++ Scripts/Actions/Examine.gd
@@ -4,6 +4,6 @@ func _ready():
 	# Set the input pickable property to true so it can receive input events
 	input_pickable = true
 
-func _on_traverse_clicked(_viewport, event, _shape_idx):
+func _on_examine_clicked(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		print("Traverse Selected")
+		print("Examine Selected")
