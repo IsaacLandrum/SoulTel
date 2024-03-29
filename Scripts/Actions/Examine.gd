0e20d3408697364d177c0c5707155dc71fdb8009
Iwantexpresso <99292736+Iwantexpresso@users.noreply.github.com>
2024-03-28 20:35:35 -0500

    Added on hover effect to each Action wheel option

--- Scripts/Actions/Examine.gd
+++ Scripts/Actions/Examine.gd
@@ -7,3 +7,11 @@ func _ready():
 func _on_examine_clicked(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		print("Examine Selected")
+		
+		
+		
+#Following section handles on hover effect		
+func _on_mouse_entered():
+	$ExamineHover.show()
+func _on_mouse_exited():
+	$ExamineHover.hide()
