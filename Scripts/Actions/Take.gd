52a8be4840ee003f5f6f48c402abb2fda4b9f712
Troy D'Amico <troydamico16@gmail.com>
2024-03-28 21:18:53 -0500

    Restricted scene switch to only when TRAVERSE has been clicked

--- Scripts/Actions/Take.gd
+++ Scripts/Actions/Take.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
+signal action(message: String)
+
 func _ready():
 	# Set the input pickable property to true so it can receive input events
 	input_pickable = true
 
 func _on_take_clicked(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		print("Take Selected")
+		emit_signal("action", "TAKE")
