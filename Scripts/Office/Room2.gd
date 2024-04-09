63fbc2da4192d2e1531e3c1f142c06d9c84a6692
czajkatony <4tonyc4@gmail.com>
2024-04-09 15:13:24 -0500

    Traverses to zoomed desk

--- Scripts/Office/Room2.gd
+++ Scripts/Office/Room2.gd
@@ -3,6 +3,8 @@ extends Node2D
 @onready var Key = $Key
 @onready var UI = $Early_UI
 @onready var Door_To_Lobby = $Door_To_Lobby
+@onready var Zoom = $Desk_Zoom
+
 @onready var Traverse = $"Early_UI/Action Wheel/Traverse"
 @onready var Talk = $"Early_UI/Action Wheel/Talk"
 @onready var Use = $"Early_UI/Action Wheel/Use"
@@ -29,6 +31,7 @@ func _ready():
 
 func _on_action(action):
 	Door_To_Lobby.setCurrentAction(action)
+	Zoom.setCurrentAction(action)
 	if !key_taken:
 		Key.setCurrentAction(action)
 
