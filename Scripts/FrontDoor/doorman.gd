36c3fe7c71f131e0ad7eeff7a878447d131500da
LiquidEnder <79119607+LiquidEnder@users.noreply.github.com>
2024-04-19 13:20:30 -0500

    Doorman dialog addition
    Added doorman dialog

--- Scripts/FrontDoor/doorman.gd
+++ Scripts/FrontDoor/doorman.gd
@@ -2,6 +2,10 @@ extends "res://Scripts/interactable.gd"
 
 signal doorman_complete
 
+var waiting = false
+var done = false
+var talkOff = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -14,6 +18,8 @@ func interact():
 		print("DOORMAN TALK")
 		emit_signal("doorman_complete")
 		DialogOut.emit(CharacterID, "01")
+		if(!done):
+			waiting = true
 		pass#TALK 
 	elif (currentAction == "EXAMINE"):
 		DialogOut.emit(CharacterID, "02")
@@ -25,4 +31,31 @@ func interact():
 	elif (currentAction == "TRAVERSE" && canTraverse):
 		DialogOut.emit(CharacterID, "04")
 		print("Traversing")
+
+func talkUpper():
+	print(talkOff)
+	talkOff+=1
+	var OutputCalc = talkOff*5+1
+	print(OutputCalc)
+	var outputStr
+	
+	if(OutputCalc < 10):
+		outputStr = "0"+str(OutputCalc)
+	else:
+		outputStr = str(OutputCalc)
+	
+	if talkOff>3:
+		waiting = false
+	else:
+		DialogOut.emit(CharacterID, outputStr)
+
+func _input(event):
+	#print("In _input")
+	# Mouse in viewport coordinates.
+	if not waiting:
+		if Input.is_action_pressed("mouse_click") && isHovering:
+			print("click")
+			interact() #takes action based on what action is selected
+	elif Input.is_anything_pressed() && not Input.is_action_pressed("mouse_click"):
+		talkUpper()
 
