8ebab98cac27ccd2aacef8bdf1421c99e0cf7c53
czajkatony <4tonyc4@gmail.com>
2024-04-19 11:40:45 -0500

    Made traversing without key safe

--- Scripts/HallwayRoom/GuestRoom1.gd
+++ Scripts/HallwayRoom/GuestRoom1.gd
@@ -24,12 +24,18 @@ func interact():
 		pass # USE
 	elif (currentAction == "TRAVERSE"):
 		DialogOut.emit(CharacterID, "04")
-		if GameManager.puzzle2.keyUsed == true:
+		if is_key_used():  # Safe checking of keyUsed
 			SceneTransition.changeGameScene("res://Scenes/GuestRoom1.tscn")
 		else:
 			print("Door is locked")
 			SoundManager.play_error_sfx()
 		
-		
+func is_key_used():
+	# Safely check if keyUsed is true in GameManager.puzzle2
+	if 'puzzle2' in GameManager and 'keyUsed' in GameManager.puzzle2:
+		return GameManager.puzzle2['keyUsed']
+	else:
+		print("Error: Puzzle2 or keyUsed not properly initialized")
+		return false	
 		
 		
